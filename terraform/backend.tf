2efecd55a0bdf98d2dac530cb1c88cc14f0e8683
BranfordTGbieor <gbieorbranford@gmail.com>
2025-04-13 02:05:45 +0200

    fix-05 for potential deploy job issues

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = var.terraform_state_bucket
-    key            = var.terraform_state_key
-    region         = var.region
-    dynamodb_table = var.terraform_lock_table
-    encrypt        = var.terraform_state_encrypt
+    bucket         = "k8s-url-shortener-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "k8s-url-shortener-terraform-locks"
+    encrypt        = true
   }
 }
